724f8596cd8b87b1a7c56866b40dc14a0d8da25a
ecppm <pyaephyomyat052@gmail.com>
2024-02-27 22:55:36 +0630

    remove wrongly placed flash message

--- resources/views/allocation.blade.php
+++ resources/views/allocation.blade.php
@@ -1,7 +1,4 @@
 <x-app-layout>
-    @if (session('success'))
-        <span class="w-full p-3 text-white bg-green-200 rounded">{{ session('success') }}</span>
-    @endif
     @livewire('allocation.allocation-page')
 </x-app-layout>
 
