0092294f2876c57f93d8dd03363ed1e37746b9dd
sawthetphyoe <sawthetphyoe28498@gmail.com>
2024-03-13 10:23:06 +0630

    student/tutor: update scheduling pages

--- resources/views/livewire/pages/tutor/tutor-meetings-page.blade.php
+++ resources/views/livewire/pages/tutor/tutor-meetings-page.blade.php
@@ -156,22 +156,34 @@
 
     <section class="w-full flex flex-col gap-6">
         <h4 class="font-semibold text-lg">Upcoming Schedule</h4>
-        <dvi class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            @foreach($pendingMeetings as $meeting)
-                <x-meeting-card :meeting="$meeting" />
-            @endforeach
-        </dvi>
+        @if(count($pendingMeetings))
+            <dvi class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                @foreach($pendingMeetings as $meeting)
+                    <x-meeting-card :meeting="$meeting" />
+                @endforeach
+            </dvi>
+        @else
+            <div class="w-full flex justify-center items-center shadow-normal rounded-[12px] bg-base-100 py-12">
+                <h4 class="font-medium text-base-content/80">No pending schedule!</h4>
+            </div>
+        @endif
     </section>
 
     <div class="divider"></div>
 
     <section class="w-full flex flex-col gap-6">
         <h4 class="font-semibold text-lg">Previous Schedule</h4>
-        <dvi class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            @foreach($finishedMeetings as $meeting)
-                <x-meeting-card :meeting="$meeting" />
-            @endforeach
-        </dvi>
+        @if(count($finishedMeetings))
+            <dvi class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                @foreach($finishedMeetings as $meeting)
+                    <x-meeting-card :meeting="$meeting" />
+                @endforeach
+            </dvi>
+        @else
+            <div class="w-full flex justify-center items-center shadow-normal rounded-[12px] bg-base-100 py-12">
+                <h4 class="font-medium text-base-content/80">No previous schedule yet!</h4>
+            </div>
+        @endif
     </section>
 
 
