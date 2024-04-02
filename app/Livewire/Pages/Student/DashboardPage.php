35cfa6dc795aaf289cc66ec316e6590e58ad20a4
ecppm <pyaephyomyat052@gmail.com>
2024-04-02 23:54:43 +0630

    add student dashboard

--- app/Livewire/Pages/Student/DashboardPage.php
+++ app/Livewire/Pages/Student/DashboardPage.php
@@ -2,12 +2,45 @@
 
 namespace App\Livewire\Pages\Student;
 
+use App\Models\File;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class DashboardPage extends Component
 {
+    public function getData()
+    {
+        $messages = Post::where(function ($query) {
+                $query->where('sender_id', Auth::user()->id)
+                    ->orWhere('receiver_id', Auth::user()->id);
+            });
+
+        $messageIds = $messages->pluck('id')->toArray();
+
+        $numberOfFiles = File::whereIn('fileable_id', $messageIds)
+            ->where('fileable_type', 'post')
+            ->count();
+
+        return [
+            "messages" => $messages->count(),
+            "files" => $numberOfFiles
+        ];
+    }
+
+    // public function getNumberOfMessages()
+    // {
+    //     return Post::where(function ($query) {
+    //             $query->where('sender_id', Auth::user()->id)
+    //                 ->orWhere('receiver_id', Auth::user()->id);
+    //         })->count();
+    // }
+
     public function render()
     {
-        return view('livewire.pages.student.dashboard-page');
+        return view('livewire.pages.student.dashboard-page', [
+            'numberOfMessages' => $this->getData()['messages'],
+            'numberOfFiles' => $this->getData()['files'],
+        ]);
     }
 }
--- resources/views/livewire/pages/student/dashboard-page.blade.php
+++ resources/views/livewire/pages/student/dashboard-page.blade.php
@@ -1,3 +1,24 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="main-container">
+    <h2 class="main-title">
+        Statistics Reports
+    </h2>
+    <div class="w-full max-w-5xl mx-auto flex flex-col sm:flex-row items-center gap-6">
+        <div
+            class="flex items-center justify-between w-full bg-base-100 rounded-xl shadow py-10 px-6">
+            <div class="flex flex-col gap-2">
+                <span class="text-sm text-base-content/75">Number of Messages</span>
+                <span class="font-semibold">{{ $numberOfMessages }}</span>
+            </div>
+            <img src="{{ asset('images/message-icon.svg') }}" alt="Send"
+                 class="w-8 h-8" />
+        </div>
+        <div class="flex items-center w-full justify-between bg-base-100 rounded-xl shadow py-10 px-6">
+            <div class="flex flex-col gap-2">
+                <span class="text-sm text-base-content/75">Number of Files</span>
+                <span class="font-semibold">{{ $numberOfFiles }}</span>
+            </div>
+            <img src="{{ asset('images/user-icon.svg') }}" alt="Send"
+                 class="w-8 h-8" />
+        </div>
+    </div>
 </div>
