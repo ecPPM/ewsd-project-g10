f12736845d4e9f30c78f07af478fba298195c3d3
ecppm <pyaephyomyat052@gmail.com>
2024-04-02 21:50:43 +0630

    add inactivity warning mail sending

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Jobs\SendMailJob;
 use App\Jobs\SendMeetingMailJob;
+use App\Jobs\SendWarningMailJob;
 
 class MailController extends Controller
 {
@@ -15,5 +16,10 @@ class MailController extends Controller
     public function sendMeetingMail($meeting, $student)
     {
         dispatch(new SendMeetingMailJob($meeting, $student));
+    }
+
+    public function sendWarningMail($student, $tutor)
+    {
+        dispatch(new SendWarningMailJob($student, $tutor));
     }
 }
---
+++ resources/views/mails/tutor-warning-mail.blade.php
@@ -0,0 +1,1 @@
+Dear {{ $body['tutor'] }}, {{ $body['student'] }}, the student under your guidance has been inactive over 28 days. Please check on them.
