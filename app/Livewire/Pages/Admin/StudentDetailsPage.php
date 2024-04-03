55f318be3432d04d3d2bb6544743ab4764648d6d
ecppm <pyaephyomyat052@gmail.com>
2024-04-03 12:25:22 +0630

    add student dashboard at details page

--- app/Livewire/Pages/Admin/StudentDetailsPage.php
+++ app/Livewire/Pages/Admin/StudentDetailsPage.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Pages\Admin;
 
+use App\Models\File;
+use App\Models\Post;
 use App\Models\User;
 use Livewire\Component;
 
@@ -15,10 +17,35 @@ class StudentDetailsPage extends Component
         $this->studentId = $studentId;
     }
 
+    public function getData()
+    {
+        $messages = Post::where(function ($query) {
+                $query->where('sender_id', $this->studentId)
+                    ->orWhere('receiver_id', $this->studentId);
+            });
+
+        $messageIds = $messages->pluck('id')->toArray();
+
+        $numberOfFiles = File::whereIn('fileable_id', $messageIds)
+            ->where('fileable_type', 'post')
+            ->count();
+
+        return [
+            "messages" => $messages->count(),
+            "files" => $numberOfFiles
+        ];
+    }
+
 
     public function render()
     {
         $student = User::where('id', $this->studentId)->first();
-        return view('livewire.pages.admin.student-details-page', ['student' => $student]);
+        $data = $this->getData();
+
+        return view('livewire.pages.admin.student-details-page', [
+            'student' => $student,
+            'numberOfMessages' => $data['messages'],
+            'numberOfFiles' => $data['files'],
+        ]);
     }
 }
--- resources/views/livewire/pages/admin/student-details-page.blade.php
+++ resources/views/livewire/pages/admin/student-details-page.blade.php
@@ -40,6 +40,25 @@
     </section>
 
     <section class="w-full h-[300px] flex justify-center items-center">
-        <h2 class="text-primary font-bold text-lg">Dashboard for each student will be displayed here.</h2>
+        <div class="w-full max-w-5xl mx-auto flex flex-col sm:flex-row items-center gap-6">
+            <div
+                class="flex items-center justify-between w-full bg-base-100 rounded-xl shadow py-10 px-6">
+                <div class="flex flex-col gap-2">
+                    <span class="text-sm text-base-content/75">Number of Messages</span>
+                    <span class="font-semibold">{{ $numberOfMessages }}</span>
+                </div>
+                <img src="{{ asset('images/message-icon.svg') }}" alt="Send"
+                     class="w-8 h-8" />
+            </div>
+            <div class="flex items-center w-full justify-between bg-base-100 rounded-xl shadow py-10 px-6">
+                <div class="flex flex-col gap-2">
+                    <span class="text-sm text-base-content/75">Number of Files</span>
+                    <span class="font-semibold">{{ $numberOfFiles }}</span>
+                </div>
+                <span>
+                    <svg fill="#0069FF" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z"/></svg>
+                </span>
+            </div>
+        </div>
     </section>
 </div>
